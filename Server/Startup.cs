f2d35274f1f2781eab1207c02ab695192e22c06c
Tomer <Tomer@TOMER>
2017-01-31 17:25:52 +0200

    Add project files.

---
+++ Server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Server.Startup))]
+
+namespace Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
