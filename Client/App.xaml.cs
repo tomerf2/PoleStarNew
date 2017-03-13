670a8677e68b217c871771ad88367ea16c0ebcb6
Tomer <Tomer@TOMER>
2017-03-13 14:49:58 +0200

    Revert "Save session data and SMS"
    This reverts commit 7874c1af7122f367b5b9644774cf0d3c2e0513ce.

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.WindowsAzure.MobileServices;
 using PoleStar.Views;
 using System;
-using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.ApplicationModel.Background;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -100,54 +98,6 @@ namespace PoleStar
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
             deferral.Complete();
-
         }
-
-        //private async Task RegisterBackgroundTask()
-        //{
-        //    //BackgroundAccessStatus backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
-
-        //    //if (backgroundAccessStatus == BackgroundAccessStatus.AllowedSubjectToSystemPolicy ||
-        //    //    backgroundAccessStatus == BackgroundAccessStatus.AlwaysAllowed)
-        //    //{
-        //    //    foreach (var bgTask in BackgroundTaskRegistration.AllTasks)
-        //    //    {
-        //    //        if (bgTask.Value.Name == "MyTask")
-        //    //        {
-        //    //            bgTask.Value.Unregister(true);
-        //    //        }
-        //    //    }
-
-        //    //    var builder = new BackgroundTaskBuilder();
-        //    //    builder.Name = "MyTask";
-        //    //    builder.SetTrigger(new TimeTrigger(15, false));
-
-        //    //    // use builder.TaskEntryPoint if you want to not use the default OnBackgroundActivated
-        //    //    // we’ll register it and now will start work based on the trigger, here we used a Time Trigger
-        //    //    BackgroundTaskRegistration task = builder.Register();
-
-        //    var builder = new BackgroundTaskBuilder();
-        //    builder.Name = "My Background Trigger";
-        //    builder.SetTrigger(new TimeTrigger(15, true));
-        //    builder.AddCondition(new SystemCondition(SystemConditionType.InternetAvailable)); //condition that internet is availible
-
-        //    // Do not set builder.TaskEntryPoint for in-process background tasks
-        //    // Here we register the task and work will start based on the time trigger.
-        //    BackgroundTaskRegistration task = builder.Register();
-
-        //}
-        ////}
-
-        //// Since we didn’t use TaskEntryPoint, this is the default constructor used
-        //protected override void OnBackgroundActivated(BackgroundActivatedEventArgs args)
-        //{
-        //    //code to run in background
-
-        //    //base.OnBackgroundActivated(args);
-        //    //DoBackgroundWork(args.TaskInstance);
-
-            
-
-        //}
     }
 }
