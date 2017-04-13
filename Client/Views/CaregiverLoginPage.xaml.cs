bb7f7ee4269897026f68e4839b4e6a78c1dbcc0e
Tomer <Tomer@TOMER>
2017-04-13 22:21:57 +0300

    Added saved login state functionality for Cargiver

--- Client/Views/CaregiverLoginPage.xaml.cs
+++ Client/Views/CaregiverLoginPage.xaml.cs
@@ -140,6 +140,7 @@ namespace PoleStar.Views
 
                         if((group.Name == txtGroupname.Text) && (group.Code == txtCode.Password))
                         {
+                            StoredData.storeCaregiverData(caregiver.Id); //store in local app data
                             this.Frame.Navigate(typeof(CaregiverMainPage), null);
                         }
                         else
